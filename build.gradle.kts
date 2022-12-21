490e7d30f40eaa7bfe918105151bb32c806606bf
Janitha Madushan <janithasen@gmail.com>
2022-12-21 15:11:16 +0530

    cleaning up.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,6 @@ plugins {
 }
 
 group = "vote.california"
-//version = "0.1.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
@@ -23,9 +22,6 @@ repositories {
 springBoot {
 	buildInfo()
 }
-
-/*extra["snippetsDir"] = file("build/generated-snippets")
-extra["testcontainersVersion"] = "1.17.6"*/
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
@@ -39,8 +35,6 @@ dependencies {
 	implementation("io.micrometer:micrometer-registry-prometheus")
 	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 	implementation("org.springframework.boot:spring-boot-starter-security")
-//	testImplementation("org.testcontainers:junit-jupiter")
-//	testImplementation("org.testcontainers:mongodb")
 }
 
 tasks.withType<KotlinCompile> {
@@ -57,10 +51,6 @@ tasks.withType<Test> {
 jib{
 	from {
 		image = "dvmarques/openjdk-17-jdk-alpine-with-timezone@sha256:cc26b502a9aa7015fc6927963a1ba4d453d76f54360df7ce9a3f32b75cb492c1"
-		/*auth {
-			username = System.getenv("DOCKER_USER")
-			password = System.getenv("DOCKER_PASSWORD")
-		}*/
 	}
 	to {
 		val sanitizedVersion = version.toString().replace('+', '_')
@@ -68,15 +58,8 @@ jib{
 		tags = setOf("latest", "$sanitizedVersion")
 	}
 	container {
-		//mainClass = "vote.california.registrationservice.RegistrationServiceApplication"
 		ports = listOf("8080/tcp", "9000/tcp")
-		//appRoot = "/app"
-		//workingDirectory = "/app"
 		creationTime = "USE_CURRENT_TIMESTAMP"
-		//jvmFlags = listOf(
-		//	"-XX:InitialRAMPercentage=40.0",
-		//	"-XX:MaxRAMPercentage=75.0",
-		//)
 	}
 }
 
@@ -84,27 +67,3 @@ task<Exec>("devVersion") {
 	val sanitizedVersion = version.toString().replace('+', '_')
 	commandLine("cmd","/c", "echo", "$sanitizedVersion",">", "dVersion")
 }
-
-//task<Exec>("updateDev") {
-//	commandLine("sed", "-i","/tag/c\\  tag: $version", "env/dev/charts/registration/values.yaml")
-//}
-
-//task<Exec>("subTreePull") {
-//    commandLine("git", "subtree", "pull", "--prefix", "env/dev/", "https://github.com/janitham/cmm707-gitops.git", "master", "--squash")
-//}
-
-//task<Exec>("subTreePush") {
-//	commandLine("git", "subtree", "push", "--prefix", "env/dev/", "https://github.com/janitham/cmm707-gitops.git", "master")
-//}
-
-//task<Exec>("gitAddAll") {
-//	commandLine("git", "add", "--all")
-//}
-
-//task<Exec>("gitCommit") {
-//	commandLine("git", "commit", "-m", "updating version")
-//}
-
-//task<Exec>("gitUser") {
- //   commandLine("git", "config", "user.name", "Action")
-//}
